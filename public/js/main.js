e370957b599e4a1422332a96bca826ae923e8749
Russ Heddleston <russelleric@gmail.com>
2012-12-13 00:06:15 -0800

    more images

--- public/js/main.js
+++ public/js/main.js
@@ -94,7 +94,7 @@ function setBackground() {
   var hours = new Date().getHours();
   var time;
   var greeting = "welcome to a little view of ";
-  var backgroundInfo = backgrounds.sample;
+  var backgroundInfo = backgrounds[0];
 
   greeting += backgroundInfo.label;
   $("div#greeting").css("color", backgroundInfo.greetingColor);
